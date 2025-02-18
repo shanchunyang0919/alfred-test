8d07e12355567b22437b3415e1f3c4b51af36aaa
shanchunyang0919 <71080192+shanchunyang0919@users.noreply.github.com>
2025-02-17 20:15:47 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-west-2a"afeawfafaef
 
   tags = {
     Name = "public_subnet"
